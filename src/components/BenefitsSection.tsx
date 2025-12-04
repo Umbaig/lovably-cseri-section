03f5f9ebae35cfad30de71a0b2b38c8cd54c4883
Umbaig <umbetova.a.t@gmail.com>
2025-12-04 19:22:18 +0100

    testc

--- src/components/BenefitsSection.tsx
+++ src/components/BenefitsSection.tsx
@@ -36,6 +36,7 @@ const BenefitsSection = () => {
           </p>
         </div>
 
+
         <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-6 max-w-6xl mx-auto">
           {benefits.map((benefit, index) => (
             <div 
